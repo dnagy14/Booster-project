3a5c9009014a15f1cdd9db22d443ac7db96ceff4
Daniel Nagy <Daniel Nagy>
2016-12-23 20:07:05 +0100

    log removed

--- src/app/Shared/filter-table.pipe.ts
+++ src/app/Shared/filter-table.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterTablePipe implements PipeTransform {
 
   transform(requests: any, term: any): any {
-    console.log(term);
     if ( term === undefined) {return requests; }
     return requests.filter( (request) => {
       return request.vehiclePlate.toLowerCase().includes(term.toLowerCase());
